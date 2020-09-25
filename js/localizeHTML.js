c2f7455ada7450723ffb563a1005e7cf492f77ad
Landius <ksusix@163.com>
2020-09-25 22:03:53 +0800

    fix<localize>: localizeHTML.js doesn't handle document.head

--- js/localizeHTML.js
+++ js/localizeHTML.js
@@ -1,13 +1,16 @@
 function localize(){
+    const head = document.head;
     const body = document.body;
-    if(body.classList.contains('localized')) return;
 
-    let html = body.innerHTML.replace(/__MSG_(.+)__/g, (match, p1)=>{
-        return chrome.i18n.getMessage(p1);
-    });
+    if(body.hasAttribute('localized')) return;
 
-    body.innerHTML = html;
-    body.classList.add('localized');
+    for(item of [head, body]){
+        item.innerHTML = item.innerHTML.replace(/__MSG_(.+)__/g, (match, p1)=>{
+            return chrome.i18n.getMessage(p1);
+        });
+    }
+
+    body.setAttribute('localized', '');
 }
 
 localize();
